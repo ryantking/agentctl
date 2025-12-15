6573bba1e4b271298d919feb53979128fc12b136
Ryan King <ryan.king@flipsidecrypto.com>
2025-12-14 21:57:10 -0500

    fix(anthropic): simplify contains helper using strings.Contains

--- internal/anthropic/errors_test.go
+++ internal/anthropic/errors_test.go
@@ -2,6 +2,7 @@ package anthropic
 
 import (
 	"errors"
+	"strings"
 	"testing"
 )
 
@@ -90,14 +91,5 @@ func TestEnhanceSDKError_MissingAPIKey(t *testing.T) {
 }
 
 func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(substr) == 0 || len(s) > len(substr) && (s[:len(substr)] == substr || s[len(s)-len(substr):] == substr || containsMiddle(s, substr)))
-}
-
-func containsMiddle(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
+	return strings.Contains(s, substr)
 }
